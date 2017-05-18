f2f48f432c4504a2c484dbe83259c4075955e792
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2017-05-18 20:31:48 +0900

    create bump-version.sh and VERSION

---
+++ bump-version.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# -*- mode: Shell-script; coding: utf-8; -*-
+# update 2017-03-19.
+
+if [ $# -ne 1 ]; then
+    echo usage: $0 VERSION
+    exit
+else
+    VERSION=$1
+fi
+
+TODAY=`date +%F`
+
+# linux's sed is gnu sed, macos not.
+if [ -e /usr/local/bin/gsed ]; then
+    SED=/usr/local/bin/gsed
+else
+    SED=`which sed`
+fi
+if [ -z ${SED} ]; then
+    echo can not find SED
+    exit
+fi
+
+${SED} -i.bak "s/^hkimura,.*$/hkimura, ${VERSION}, ${TODAY}./" cgi/gimeo.cgi
+
+# example of sed 'c' command.
+#${SED} -i.bak "/(defvar \*version\*/ c\
+#(defvar *version* \"${VERSION}\")" *.lisp
+
+echo ${VERSION} > VERSION
